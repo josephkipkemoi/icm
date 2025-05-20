89abe0012a3534f3aca3e80424a1dfe250edf97b
jkemboe <jkemboe@gmail.com>
2025-05-20 12:12:48 +0300

    scroll to top

--- src/pages/gift-cards.js
+++ src/pages/gift-cards.js
@@ -1,15 +1,27 @@
+import { useEffect, useRef } from "react"
 import Footer from "../components/footer"
 import Header from "../components/header"
 import WhatsAppComponent from "../components/whatsapp"
+import { useLocation } from "react-router-dom"
 
 const GiftCardsPage = () => {
+    const topRef = useRef(null)
+    const {pathname} = useLocation()
+    const scrollToTop = () => {
+        topRef.current?.scrollIntoView({ behavior: 'smooth' })
+    }
+
+    useEffect(() => {
+        scrollToTop()
+    }, [pathname])
+
     return (
-        <>
+        <div ref={topRef}>
             <Header/>
             <GiftCardComponent/>
             <WhatsAppComponent/>
             <Footer/>
-        </>
+        </div>
     )
 }
 
