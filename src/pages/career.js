d6ee7b94eb195a846affc9271959497337652f30
jkemboe <jkemboe@gmail.com>
2025-05-20 11:54:38 +0300

    scroll to top

--- src/pages/career.js
+++ src/pages/career.js
@@ -1,9 +1,21 @@
+import { useEffect, useRef } from "react"
 import Footer from "../components/footer"
 import Header from "../components/header"
+import { useLocation } from "react-router-dom"
 
 const CareerPage = () => {
+    const topRef = useRef(null)
+    const {pathname} = useLocation()
+    const scrollToTop = () => {
+        topRef.current?.scrollIntoView({ behavior: 'smooth' })
+    }
+    
+    useEffect(() => {
+        scrollToTop()
+    }, [pathname])
+
     return (
-        <div>
+        <div ref={topRef}>
             <Header/>
             <CareerPageComponent/>
             <Footer/>
